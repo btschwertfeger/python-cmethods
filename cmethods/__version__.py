853668374016fdc281626075c36427833fd5b3c6
Benjamin Thomas Schwertfeger <development@b-schwertfeger.de>
2023-03-04 19:51:14 +0100

    added unittests

--- cmethods/__version__.py
+++
@@ -1,3 +0,0 @@
-'''version of this module'''
-VERSION = (0,6,1)
-__version__ = '.'.join(map(str, VERSION))
---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Copyright (C) 2023 Benjamin Thomas Schwertfegerr
+# Github: https://github.com/btschwertfeger
+#
