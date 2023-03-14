9a90fe1701299aa8ce12f1e1a712e4fda184b254
Benjamin Thomas Schwertfeger <development@b-schwertfeger.de>
2023-03-14 18:09:26 +0100

    create v0.6.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/btschwertfeger/Bias-Adjustment-Python"
 EMAIL = "development@b-schwertfeger.de"
 AUTHOR = "Benjamin Thomas Schwertfeger"
 REQUIRES_PYTHON = ">=3.8.0"
-VERSION = "0.6.1"
+VERSION = "0.6.2"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
