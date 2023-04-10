f1ae18279a3c7a0eee71a476ce400412ae88cfa2
Benjamin Thomas Schwertfeger <development@b-schwertfeger.de>
2023-04-10 15:16:21 +0200

    added the documentation using sphinx

---
+++ docs/conf.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+"""This module is the configuration for the Sphinx documentation building process"""
+# pylint: disable=invalid-name
+
+import os
+import sys
+
+project = "python-cmethods"
+copyright = "2023, Benjamin Thomas Schwertfeger"  # pylint: disable=redefined-builtin
+author = "Benjamin Thomas Schwertfeger"
+
+
+# to import the package
+sys.path.insert(0, os.path.abspath(".."))
+
+# import links
+rst_epilog = ""
+# Read link all targets from file
+with open("links.rst", encoding="utf-8") as f:
+    rst_epilog += f.read()
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.mathjax",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "links.rst"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+html_context = {
+    "display_github": True,
+    "github_user": "btschwertfeger",
+    "github_repo": "python-cmethods",
+    "github_version": "master/docs/",
+}
+# html_theme_options = {
